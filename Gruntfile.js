c4cdcc7993295fd80361625207e26eb07e8126ae
Lou Quillio <quillio@gmail.com>
2017-10-17 17:32:21 -0700

    Fix `grunt replace` task.
    Remove extraneous grunt plugins.
    
    TODO: Reposition syntax element type/size at column 80
    throughour document.
    
    TODO: Integrate grunt replace, jekyll build and
    jekyll serve.
    
    Change-Id: I5ec2c455cde5f41b3acdd67fb8087bc4219b9e3b

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,87 +1,40 @@
-/**
- * This Gruntfile is used to build the scripts.
- */
-
-/*jshint
-    unused: true,
-    debug: true,
-    devel: true,
-    browser: true,
-    asi: true
- */
-
-
 module.exports = function( grunt ) {
-
     // Initial grunt configurations
     grunt.initConfig({
-
-
         // Read the package manifest.
-        pkg: grunt.file.readJSON( 'package.json' ),
-
-
-        // Concatenate the files and add banners.
-        concat: {
+        pkg: grunt.file.readJSON( "package.json" ),
+        replace: {
+          dist: {
             options: {
-                banner: '/*!\n' +
-                        ' * <%= pkg.title %> v<%= pkg.version %>, <%= grunt.template.today("yyyy-mm-dd") %>\n' +
-                        ' *\n' +
-                        ' * Hosted on <%= pkg.homepage %>\n' +
-                        ' * Copyright (c) <%= pkg.author.name %> (<%= pkg.author.url %>)\n' +
-                        ' * Licensed under <%= pkg.licenses[ 0 ].type %> license.\n' +
-                        ' */\n\n'
+              patterns: [
+                {
+                  match: /@@[a-zA-Z_0-9]*(\[[^\]]*\])*/g,
+                  replacement: function(match) { return "<b>" + match.substring(2) + "</b>"; }
+                }
+              ]
             },
-            scripts: {
-                src: '_scripts/*.js',
-                dest: 'media/js/main.js'
+            files: [
+              {src: ["_site/index.html"], dest: "_site/av1-spec.html"}
+            ]
+          }
+        },
+        jekyll: {
+          options: {
+            bundleExec: true,
+            skip_initial_build: false,
+            verbose: true,
+          },
+          serve: {
+            options: {
+              serve: true,
+              dest: ".jekyll",
             }
-        },
-
-
-        // Lint the build files.
-        jshint: {
-            files: [ 'media/js/main.js' ]
-        },
-
-
-        // Watch the project files.
-        watch: {
-            files: [ '_scripts/*.js' ],
-            tasks: [ 'default' ]
+          }
         }
     }) //grunt.initConfig
-
-
     // Load the NPM tasks.
-    grunt.loadNpmTasks( 'grunt-contrib-concat' )
-    grunt.loadNpmTasks( 'grunt-contrib-watch' )
-    grunt.loadNpmTasks( 'grunt-contrib-jshint' )
-
-
+    grunt.loadNpmTasks( "grunt-jekyll" );
+    grunt.loadNpmTasks( "grunt-replace" );
     // Register the default tasks.
-    grunt.registerTask( 'default', [ 'jshint' ] )
-
-    grunt.loadNpmTasks('grunt-replace');
-    grunt.initConfig({
-      replace: {
-        dist: {
-          options: {
-            patterns: [
-              {
-                match: /@@[a-zA-Z_0-9]*(\[[^\]]*\])*/g,
-                replacement: function(match) { return "<b>" + match.substring(2) + "</b>"; }
-              }
-            ]
-          },
-          files: [
-            {src: ['_site/index.html'], dest: '_site/av1-spec.html'}
-          ]
-        }
-      }
-    });
-    grunt.registerTask('default', 'replace');
-
+    grunt.registerTask( "default", "replace");
 } //module.exports
-
-
